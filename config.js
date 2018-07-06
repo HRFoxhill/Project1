0f7c783f10ddcbacab5241b0f4ed63cef6b8198b
Eddie77 <edisonyang527@gmail.com>
2018-07-06 13:31:34 -0500

    remove content from gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+// Ref link https://gist.github.com/derzorngottes/3b57edc1f996dddcab25
+
+var config = {
+    APIKEY_CV : 'f9be3c08fb84c5acd562bffdc4a02e8ee1092509',
+    TOKEN_SH : '10160469589180514',    
+  }
+
